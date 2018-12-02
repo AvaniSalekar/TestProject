5e21f1d515e52e20074f8d90a751e211f07e45b4
Avani Salekar <Avani Salekar@LAPTOP-BB33G5GM>
2018-12-02 18:12:00 +0530

    Add project files.

---
+++ TestProjectForGitHub/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestProjectForGitHub.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TestProjectForGitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestProjectForGitHub.WebApiApplication" Language="C#" %>
